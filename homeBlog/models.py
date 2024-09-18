45e2a5c803efa6cfff19ddf8c83c21730f7ee829
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-18 13:37:47 +0000

    added models to mu homeBlog/models

--- homeBlog/models.py
+++ homeBlog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="blog_posts"
+)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+
+    # i am also going to need 
+    # featured_image = CloudinaryField('image', default='placeholder')
+    # likes = models.ManyToManyField(User, related_name='hike_likes', blank=True)
