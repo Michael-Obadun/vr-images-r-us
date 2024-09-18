7511a4c20a75da335142998d385549fa426d8e27
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-18 15:19:10 +0000

    created comments model and migration done and also adjusted homeBlog/admin

--- homeBlog/models.py
+++ homeBlog/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+# i am also going to need 
+    # featured_image = CloudinaryField('image', default='placeholder')
+    # likes = models.ManyToManyField(User, related_name='hike_likes', blank=True)
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
@@ -14,8 +18,18 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+   
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    # approved model may have to be deleted
 
 
-    # i am also going to need 
-    # featured_image = CloudinaryField('image', default='placeholder')
-    # likes = models.ManyToManyField(User, related_name='hike_likes', blank=True)
+
+
